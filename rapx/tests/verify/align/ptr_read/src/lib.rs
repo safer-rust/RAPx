2a089934f71bc92931fe638fb95730978967a68b
XU, Hui <china.xuhui@gmail.com>
2026-05-06 21:31:49 +0800

    add 2 test cases for alignment verification (#240)

---
+++ rapx/tests/verify/align/ptr_read/src/lib.rs
@@ -0,0 +1,22 @@
+#![feature(register_tool)]
+#![register_tool(rapx)]
+
+#[rapx::verify]
+fn safe_read<T>(ptr: *const T) -> Option<u32> {
+    if ptr % std::mem::align_of::<u32>() == 0 {
+        unsafe {
+            let p = ptr as *const u32;
+            Some(p.read())
+        }
+    } else {
+        None
+    }
+}
+
+#[rapx::verify]
+fn unsafe_read<T>(ptr: *const T) -> u32 {
+    unsafe {
+        let p = ptr as *const u32;
+        p.read()
+    }
+}
